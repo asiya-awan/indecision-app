32983475313cd90bcc36b5a56c4647146de057b3
asiya-awan <asia.kainat@hotmail.com>
2019-08-10 15:44:12 -0500

    setup production and build server

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,67 @@
 const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: "none", // "production" | "development" | "none"
-  // Chosen mode tells webpack to use its built-in optimizations accordingly.
-  entry: "./src/app.js", // string | object | array
-  // defaults to ./src
-  // Here the application starts executing
-  // and webpack starts bundling
-  output: {
-    // options related to how webpack emits results
-    path: path.join(__dirname, "public"), // string
-    // the target directory for all output files
-    // must be an absolute path (use the Node.js path module)
-    filename: "bundle.js", // string
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    open: 'chrome'
+
+module.exports = (env) => {
+  console.log("env: get env value from build:prod script(--env production): ", env);
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+  return {
+
+    entry: ['babel-polyfill','./src/app.js'],
+  
+    output: {
+      path: path.join(__dirname, 'public','dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+      }]
+    },
+    plugins: [
+      CSSExtract,
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID' : JSON.stringify(process.env.FIREBASE_APP_ID)
+
+      })
+		],
+    devtool: isProduction?'source-map': 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      open: 'chrome',
+      historyApiFallback: true
+    }
   }
 };
